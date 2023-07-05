2bb59bb8fdba8a5862af62bf110dd812582045bc
michaelangelovalente <miky10.mv@gmail.com>
2023-07-05 10:45:26 +0200

    Rest api -> product & product category

--- src/main/java/com/example/ecommerce/entity/Product.java
+++ src/main/java/com/example/ecommerce/entity/Product.java
@@ -4,6 +4,8 @@ package com.example.ecommerce.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,7 +14,8 @@ import java.util.Date;
 
 @Entity
 @Table(name="product")
-@Data
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
